355916e9e0c77f48d5173c6598a61a7e7c15e910
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2024-06-05 23:44:24 +0530

    Updated isLoggedIn and some changes

--- server/middleware/isLoggedIn.js
+++ server/middleware/isLoggedIn.js
@@ -3,25 +3,24 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
 const isLoggedIn = async (req, res, next) => {
-  const authorizationHeader = req.headers.authorization;
-  const token = authorizationHeader && authorizationHeader.split(' ')[1];
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    res.status(404).json({ message: 'Invalid access token' });
+  }
 
   try {
-    if (!token) {
-      throw new Error('Access token is missing');
-    }
-
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decodedToken.id);
+    const user = await User.findById(decodedToken.id).select('-password');
 
     if (!user) {
-      throw new Error('User not found');
+      res.status(404).json({ message: 'Invalid access token' });
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401, error.message);
+    res.status(404).json({ message: 'Invalid access token' });
   }
 };
 
--- server/routes/privateRoute.js
+++ server/routes/privateRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const privatePage = require('../controller/privatePage');
 const { isLoggedIn } = require('../middleware/isLoggedIn');
 
-router.get('/private', isLoggedIn, privatePage);
+router.get('/private', isLoggedIn, privatePage); // Pass isLoggedIn as a middleware function reference
 
 module.exports = router;
