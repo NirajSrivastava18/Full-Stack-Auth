a73a9c768bd62ca5ee5c1844faf2d966512ed1f5
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2024-06-08 16:07:48 +0530

    minor changes

--- client/src/components/screens/Register.js
+++ client/src/components/screens/Register.js
@@ -22,7 +22,7 @@ const Register = () => {
       .required('Email is required'),
     password: Yup.string()
       .required('Password is required')
-      .min(8, 'Password must be at least 8 characters long'),
+      .min(8, 'Password must be at least 6 characters long'),
     confirmPassword: Yup.string()
       .required('Confirm password is required')
       .oneOf([Yup.ref('password'), null], 'Passwords not matched'),
