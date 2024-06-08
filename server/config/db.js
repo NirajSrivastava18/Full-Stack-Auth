b1db8ce2e39fa549adf41bb37a57edd60bfc7c79
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2024-06-08 16:21:12 +0530

    minor env changes

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose
-      .connect('mongodb+srv://Auth:Auth1234@cluster0.rkqwd.mongodb.net/Auth')
+      .connect(process.env.MONGO_URI)
       .then(() => console.log('MongoDB Connected Successfully'));
   } catch (err) {
     console.log(err.message);
